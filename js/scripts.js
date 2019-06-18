ff09b0c59e5ba95d7027b3f3c2b6a0a1281adfdd
Jordan King and Jaime Takikawa <jordan@>
2019-06-17 20:48:35 -0700

    add palindrome functionality

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+// business logic
+var outputStrings = [];
+for (var i = 0, n = inputValues.length; i < n; ++i) {
+  outputStrings.push(String(inputValues[i]));
+}
+var reverseString = outputStrings.reverse()
+
+
+
+
+
+
+front end logic
+$(document).ready(function(event) {
+  event.preventDefault();
+
+  var inputword = ("input#word")
+  var reverseword = inputword.charAt()
+})
+// function palindromeChecker(text){
+//   var textLen = text.length;
+//   for (var i = 0; i < textLen/2; i++) {
+//     if (text[i] !== text[textLen -1 - i]) {
+//     return false;
+//   }
+// }
+//   return true;
+// }
